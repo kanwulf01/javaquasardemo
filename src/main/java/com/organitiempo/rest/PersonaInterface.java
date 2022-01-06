f17c245e9f98b99ce83ae8f1976f0fdb2c81b557
kanwulf01 <radamanthyzdark@hotmail.com>
2022-01-06 09:31:15 -0500

    build files

---
+++ src/main/java/com/organitiempo/rest/PersonaInterface.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.organitiempo.rest;
+import java.util.List;
+/**
+ *
+ * @author Lenovo
+ */
+public interface PersonaInterface {
+    List<Persona> listar();
+    Persona listarId(int id);
+    Persona add(Persona p);
+    Persona edit(Persona p);
+    Persona delete(int id);
+}
---
+++ src/main/java/com/organitiempo/rest/PersonaRepositorie.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.organitiempo.rest;
+
+import org.springframework.data.repository.Repository;
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface PersonaRepositorie extends Repository<Persona, Integer> {
+        
+}
